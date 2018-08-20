74f07469033f914d81d92fe129df72b8301608f3
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 07:28:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,15 +11,14 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("el")
-end
+  array.unshift(element)
 
 def remove_element_from_end_of_array(array)
-  array.pop("el")
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift("el5")
+  array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
